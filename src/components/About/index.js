fb789d45bd862cd605c84a942eb953e0650c7de6
Sabrina Kennedy <skenne21@gmail.com>
2018-06-19 14:45:06 -0600

    Adding info about me to About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Header from '../Header/';
+
 
 const About = () => {
   return(
     <div className='About'>
-      
+      <Header />
+      <div>
+        <h3>WHO AM I AS A PERSON?</h3>
+        <p>As a Colorado Native, I love spending my free time in the mountains
+         no matter the activity or the type of shoes I have on my feet. I also love
+         a great cup of coffee, a good book, or listening to a great podcast.
+        </p>
+        <p>My path as a Front-End Developer has lead me to focus on Javascript. I love
+        the diversity and flexibility that Javascript allows when I am innovating my
+        ideas into reality with code.
+        </p>
+        <p>I was driven towards a career in web development because I love the challenge
+        and the reward of figuring out a problem or learning a new language.
+        As a former Trainer, I loved working with clients and helping them solve
+        there own barriers with fitness and nutrition. I was invested into their
+        goals, I bookmarked my own success by seeing and driving my clients to reach
+         their goals. I bring this same drive and passion into the code I create. I
+         bookmark a completion of a project as a personal win and get fully invested
+         into seeing the full lifecycle of a project or idea. I am driven to create
+         and innovate code that makes a meaningful impact on community and individuals
+         lives.
+         </p>
+      </div>
     </div>
   )
 }
